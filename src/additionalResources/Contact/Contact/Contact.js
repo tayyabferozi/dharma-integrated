29229d3fe8c25961a8cfc00238f576424681c8ad
= <=>
2020-07-11 02:01:08 -0700

    Add material form fields

--- src/additionalResources/Contact/Contact/Contact.js
+++ src/additionalResources/Contact/Contact/Contact.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import './Contact.css';
 import Map from '../UI/Map/Map';
 
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 180
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2)
+  }
+}));
+
 const Contact = () => {
+  const classes = useStyles();
+  const [status, setStatus] = React.useState('');
+  const [enquiry, setEnquiry] = React.useState('');
+
+  const handleStatusChange = event => {
+    setStatus(event.target.value);
+  };
+
+  const handleEnquiryChange = event => {
+    setEnquiry(event.target.value);
+  };
+
   return (
     <div className="Contact">
       <h1>
@@ -108,27 +137,58 @@ const Contact = () => {
       <div className="ContactForm">
         <div className="FormControl">
           <h6>I am a</h6>
-          <select>
+          {/* <select>
             <option>User</option>
             <option>Retailer</option>
-          </select>
+          </select> */}
+          <FormControl className={classes.formControl}>
+            <InputLabel id="status">Status</InputLabel>
+            <Select
+              labelId="status"
+              id="status"
+              value={status}
+              onChange={handleStatusChange}>
+              <MenuItem value={'user'}>User</MenuItem>
+              <MenuItem value={'retailer'}>Retailer</MenuItem>
+            </Select>
+          </FormControl>
         </div>
         <div className="FormControl">
           <h6>Email</h6>
-          <input type="email" />
+          {/* <input type="email" /> */}
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
         </div>
         <div className="FormControl">
           <h6>Tell us more about your Enquiry</h6>
-          <select>
+          {/* <select>
             <option>Products</option>
             <option>Onboarding</option>
             <option>Refund</option>
             <option>Other</option>
-          </select>
+          </select> */}
+          <FormControl className={classes.formControl}>
+            <InputLabel id="enquiry">Enquiry</InputLabel>
+            <Select
+              labelId="enquiry"
+              id="enquiry"
+              value={enquiry}
+              onChange={handleEnquiryChange}>
+              <MenuItem value={'products'}>Products</MenuItem>
+              <MenuItem value={'onboarding'}>Onboarding</MenuItem>
+              <MenuItem value={'refund'}>Refund</MenuItem>
+              <MenuItem value={'other'}>Other</MenuItem>
+            </Select>
+          </FormControl>
         </div>
         <div className="FormControl">
           <h6>Message</h6>
-          <textarea />
+          {/* <textarea /> */}
+          <TextareaAutosize
+            aria-label="empty textarea"
+            rowsMin={5}
+            colsMin={5}
+            placeholder="Empty"
+          />
         </div>
       </div>
     </div>
